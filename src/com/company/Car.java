67b753c6a4f4631f4c23707486556ac47ba4fd29
przemyslaw wendeholm <pwenderholm@gmai.com>
2021-02-08 22:05:24 +0100

    after var exercise

---
+++ src/com/company/Car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Car {
+    public static void main(String[] args) {
+        String carName = "Toyota";
+        String carModel = "Avensis";
+        int carYear = 2001;
+        double carPrice = 29000.0;
+
+        System.out.println("Nazwa auta to " + carName + " model to " + carModel + " rok produkcji to " + carYear + " cena auta to " + carPrice);
+    }
+}
---
+++ src/com/company/DataType.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class DataType {
+    public static void main(String[] args) {
+        System.out.print("Liczba int: ");
+        System.out.print(25);
+        System.out.print('\n');
+
+        System.out.print("Liczba double: ");
+        System.out.print(25.75);
+        System.out.print('\n');
+
+        System.out.print("Znak A: ");
+        System.out.print('A');
+        System.out.print('\n');
+
+        System.out.print("Czy uczę się programowania? ");
+        System.out.println(true);
+
+        System.out.println("-Cześć Jan, słyszałem, że masz " + 25 + " lat i uczysz się programowania. Czy to prawda?");
+        System.out.println("-" + true);
+        System.out.println("-Czy Twoje inicjały to J K?");
+        System.out.println("-Tak," + true);
+    }
+}
