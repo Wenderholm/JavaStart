eec2366b83e6c9c39deabd332d7c671269cfcd1c
przemyslaw wendeholm <pwenderholm@gmai.com>
2021-07-12 14:42:00 +0200

    trening polimorfizmu

---
+++ src/polimorf/Doctor.java
@@ -0,0 +1,22 @@
+package polimorf;
+
+public class Doctor extends Person{
+    private double bonus;
+
+    public Doctor(String firstName, String secondName, double salary, double bonus) {
+        super(firstName, secondName, salary);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getInfo(){
+        return super.getInfo() + ",Premia: " + getBonus();
+    }
+}
---
+++ src/polimorf/Hospital.java
@@ -0,0 +1,24 @@
+package polimorf;
+
+public class Hospital {
+    private static final int MAX_EMPLOYEES = 3;
+    Person[] persons = new Person[MAX_EMPLOYEES];
+    private int employeesNumber = 0;
+
+    void add(Person person){
+        if(employeesNumber < MAX_EMPLOYEES){
+            persons[employeesNumber] = person;
+            employeesNumber++;
+        }
+    }
+
+    String getInfo(){
+        String result = "";
+        for(int i = 0; i<employeesNumber; i++){
+            result = result + persons[i].getInfo() + "\n";
+        }
+        return result;
+    }
+
+
+}
