88a1a447efac3c727f60857bd2f16ba3e9cfaa62
przemyslaw wendeholm <pwenderholm@gmai.com>
2021-10-03 15:36:45 +0200

    zadanie sprawdzanie wprowadznych słów i usuwanie zawierających podane słowo

---
+++ src/zbiory/zad2dodawanie_produktu/Main.java
@@ -0,0 +1,11 @@
+package zbiory.zad2dodawanie_produktu;
+
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductManager productManager = new ProductManager();
+        productManager.mainLoop();
+    }
+
+}
